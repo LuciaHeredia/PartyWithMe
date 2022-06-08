f6aa5665792a9f8200052053dd5143bd73feccbf
Lucia Heredia <luciaheredia22@gmail.com>
2022-06-08 16:45:23 +0300

    Initial Commit

---
+++ PartyWithMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyWithMe
+//
+//  Created by lucia heredia on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
