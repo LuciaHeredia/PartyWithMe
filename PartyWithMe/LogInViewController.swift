f1291d0306292e75f7a132107369f79bf63ca3d1
Lucia Heredia <luciaheredia22@gmail.com>
2022-06-08 20:55:12 +0300

    added new ViewControllers

---
+++ PartyWithMe/LogInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogInViewController.swift
+//  PartyWithMe
+//
+//  Created by lucia heredia on 08/06/2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ PartyWithMe/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  PartyWithMe
+//
+//  Created by lucia heredia on 08/06/2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
