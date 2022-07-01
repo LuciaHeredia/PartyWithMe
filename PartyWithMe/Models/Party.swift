b42904eb7e4cfdc88230c100041b07023cdd44f3
Lucia Heredia <luciaheredia22@gmail.com>
2022-07-01 13:01:13 +0300

    party model

---
+++ PartyWithMe/Models/Party.swift
@@ -0,0 +1,22 @@
+//
+//  Party.swift
+//  PartyWithMe
+//
+//  Created by lucia heredia on 01/07/2022.
+//
+
+import Foundation
+
+struct Party: Codable {
+    var name: String
+    var date: String
+    var city: String
+    
+    var totalAmount: Int
+    var currentAmount: Int
+    
+    var description: String
+    
+    var idListOfPeople: String
+    var idPicture: String
+}
